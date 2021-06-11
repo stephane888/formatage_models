8def5460b17bf7bee03cc0024ca45f99977ad97f
stephane888 <kksasteph888@gmail.com>
2021-06-11 06:30:36 +0100

    MAJ

--- src/Plugin/Layout/Teasers/FormatageModelsBlogTeaserImgDesc.php
+++ src/Plugin/Layout/Teasers/FormatageModelsBlogTeaserImgDesc.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Drupal\formatage_models\Plugin\Layout\Teasers;
 
 use Drupal\formatage_models\Plugin\Layout\FormatageModels;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * A very advanced custom layout.
@@ -34,4 +36,36 @@ use Drupal\formatage_models\Plugin\Layout\FormatageModels;
  * )
  */
 class FormatageModelsBlogTeaserImgDesc extends FormatageModels {
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration() {
+        return parent::defaultConfiguration() + [
+          'limit_text' => 150
+        ];
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+        $form = parent::buildConfigurationForm($form, $form_state);
+        $form['limit_text'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Limiter le texte de description'),
+          '#default_value' => $this->configuration['limit_text'],
+          '#description' => 'si la valeur est vide le texte va etre afficher dans son enssemble, si non les balise sont supprimées et le nombre de charactere est affiché.'
+        ];
+        $this->Layouts->buildConfigurationForm($form);
+        return $form;
+    }
+
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+        parent::submitConfigurationForm($form, $form_state);
+        $this->configuration['limit_text'] = $form_state->getValue('limit_text');
+    }
+
 }
