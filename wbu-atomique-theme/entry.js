130aa2b47b1f33b9f27eee81560254625b9369b3
stephane888 <kksasteph888@gmail.com>
2021-10-17 21:18:17 +0200

    MAJ

--- wbu-atomique-theme/entry.js
+++ wbu-atomique-theme/entry.js
@@ -1,22 +1,4 @@
 module.exports = {
-  "formatage-models-textcotebg":
-    "./src/js/sections/formatage-models-textcotebg.js",
-  formatage_models_swiper: "./src/js/sections/formatage_models_swiper.js",
-  "formatage-models-titre-description":
-    "./src/js/sections/formatage-models-titre-description.js",
-  formatage_models_swiper_big:
-    "./src/js/sections/formatage_models_swiper_big.js",
-  "formatage-models-comments": "./src/js/sections/formatage-models-comments.js",
-  formatage_models_swiper_unique:
-    "./src/js/sections/formatage_models_swiper_unique.js",
-  "formatage-models-teaser-small":
-    "./src/js/teasers/formatage-models-teaser-small.js",
-  "formatage-models-cardsimple":
-    "./src/js/teasers/formatage-models-cardsimple.js",
-  formatage_models_swiper_big_v2:
-    "./src/js/sections/formatage_models_swiper_big_v2.js",
-  "formatage-models-text-hover":
-    "./src/js/teasers/formatage-models-text-hover.js",
-  formatage_models_swiper_big_v3:
-    "./src/js/sections/formatage_models_swiper_big_v3.js",
+  "formatage-models-footer1":
+    "./src/js/sections/footers/formatage-models-footer1.js",
 };
