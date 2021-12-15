cee185c65c8748613b0b40ef2a1c6521d5996c22
stephane888 <kksasteph888@gmail.com>
2021-12-15 11:13:46 +0100

    MAJ3

--- src/Plugin/Layout/FormatageModelsBlogTeaserSvgTitreSvg.php
+++ src/Plugin/Layout/FormatageModelsBlogTeaserSvgTitreSvg.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Drupal\formatage_models\Plugin\Layout;
 
-use Drupal\Core\Layout\LayoutDefault;
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\formatage_models\Plugin\Layout\Teasers\FormatageModelsTeasers;
 
 /**
  * A very advanced custom layout.
@@ -31,45 +33,58 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class FormatageModelsBlogTeaserSvgTitreSvg extends LayoutDefault {
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public function defaultConfiguration() {
-		return parent::defaultConfiguration() + [
-			'css' => ''
-		];
-	}
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-		$form = parent::buildConfigurationForm($form, $form_state);
-		$form['label']['#default_value'] = empty($this->configuration['label']) ? $this->getBaseId() : $this->configuration['label'];
-		$form['css'] = [
-			'#type' => 'textfield',
-			'#title' => $this->t('Class css'),
-			'#default_value' => $this->configuration['css']
-		];
-		return $form;
-	}
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {}
-
-	/**
-	 *
-	 * {@inheritdoc}
-	 */
-	public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-		parent::submitConfigurationForm($form, $form_state);
-		$this->configuration['css'] = $form_state->getValue('css');
-	}
+class FormatageModelsBlogTeaserSvgTitreSvg extends FormatageModelsTeasers {
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+    // TODO Auto-generated method stub
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'formatage_models') . "/icones/teasers/formatage-models-teaser-svg-titre-text.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => ''
+    ];
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['label']['#default_value'] = empty($this->configuration['label']) ? $this->getBaseId() : $this->configuration['label'];
+    $form['css'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Class css'),
+      '#default_value' => $this->configuration['css']
+    ];
+    return $form;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['css'] = $form_state->getValue('css');
+  }
+  
 }
