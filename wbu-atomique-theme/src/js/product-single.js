7f46dfb23967fcd57937351a1fb8ff39fbe10afc
noumecha <noumechaivan@gmail.com>
2022-05-11 13:15:27 +0100

    maj : by spaker --the tmc

--- wbu-atomique-theme/src/js/product-single.js
+++ wbu-atomique-theme/src/js/product-single.js
@@ -1,2 +1,2 @@
 import tab from "@stephane888/wbu-atomique/js/products/product-all-description.js";
-import "../scss/product-single.scss";
+import "@stephane888/wbu-atomique/wbu-atomique/scss/product-single.scss";
