9bd13dbf2dac51a321786665eb773300ee68e389
stephane888 <kksasteph888@gmail.com>
2022-06-05 20:52:13 +0200

    MAJ

--- wbu-atomique-theme/entry.js
+++ wbu-atomique-theme/entry.js
@@ -1,24 +1,4 @@
 module.exports = {
-  "formatage-models-hero-ac": "./src/js/sections/formatage-models-hero-ac.js",
-  "formatage-models-about-ac": "./src/js/sections/formatage-models-about-ac.js",
-  "formatage-bloc-price": "./src/js/teasers/formatage-bloc-price.js",
-  "formatage-models-list-section":
-    "./src/js/teasers/formatage-models-list-section.js",
-  "formatage-models-titre-rc": "./src/js/teasers/formatage-models-titre-rc.js",
-  "formatage-models-hero2": "./src/js/sections/formatage-models-hero2.js",
-  "formatage-models-hero-ason":
-    "./src/js/sections/formatage-models-hero-ason.js",
-  "formatage-models-fb-feature":
-    "./src/js/sections/formatage-models-fb-feature.js",
-  "formatage-models-fb-service":
-    "./src/js/sections/formatage-models-fb-service.js",
-  "formatage-models-hero-lgb": "./src/js/sections/formatage-models-hero-lgb.js",
-  "formatage-models-left-right":
-    "./src/js/sections/formatage-models-left-right.js",
-  "formatage-models-type-parquet":
-    "./src/js/sections/formatage-models-type-parquet.js",
-  "formatage-models-hero-saint-gobain":
-    "./src/js/sections/formatage-models-hero-saint-gobain.js",
-  "formatage-models-price-saint-gobain":
-    "./src/js/sections/formatage-models-price-saint-gobain.js",
+  "formatage_models_admin_paragraph": "./src/js/formatage_models_admin_paragraph.js",
+  "formatage-models-admin": "./src/js/formatage-models-admin.js",
 };
---
+++ wbu-atomique-theme/src/js/formatage_models_admin_paragraph.js
@@ -0,0 +1,1 @@
+import "../scss/formatage_models_admin_paragraph.scss";
